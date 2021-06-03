bc1d55d3decb01555dc2556b779e73a52ca208d2
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2021-06-03 17:39:03 +0200

    Expand vector visualization support
    Factor out single vector draw method and allow for supplying a custom vector object diameter.

--- Assets/VectorVisualizer.cs
+++ Assets/VectorVisualizer.cs
@@ -11,26 +11,32 @@ public class VectorVisualizer {
 		this.offset = offset;
 	}
 
-	public void visualize(Vec3D[] positions, VecD vectors, double scale = 1d) {
+	public void visualize(Vec3D[] positions, VecD vectors, float scale = 1f) {
 		this.visualize(new VecD(positions), vectors, scale);
 	}
 
-	public void visualize(VecD positions, VecD vectors, double scale = 1d) {
-		float vecThicknessScale = 0.02f;
+	public void visualize(VecD positions, VecD vectors, float scale = 1f) {
+		float vecThickness = 0.02f;
 		for(int i = 0; i < positions.length / 3; i++) {
-			Vector3 origin = new Vector3((float) positions[3 * i], (float) positions[3 * i + 1], (float) positions[3 * i + 2]) + offset;
-			Vector3 vec = new Vector3((float) vectors[3 * i], (float) vectors[3 * i + 1], (float) vectors[3 * i + 2]) * (float) scale;
-			float vecMag = vec.magnitude;
-			if(vecMag != 0f) {
-				GameObject gameObj = GameObject.CreatePrimitive(PrimitiveType.Cylinder); // Defaults to 2m height, 1m diameter, origin at center.
-				Vector3 objMiddle = origin + vec / 2f;
-				gameObj.transform.position = objMiddle;
-				Vector3 rotAxis = Vector3.Cross(new Vector3(0f, 1f, 0f), vec);
-				float angle = Vector3.SignedAngle(new Vector3(0f, 1f, 0f), vec, rotAxis);
-				gameObj.transform.rotation = Quaternion.AngleAxis(angle, rotAxis);
-				gameObj.transform.localScale = new Vector3(vecThicknessScale, 0.5f * vecMag, vecThicknessScale);
-				this.vectorGameObjects.Add(gameObj);
-			}
+			Vector3 origin = new Vector3((float) positions[3 * i], (float) positions[3 * i + 1], (float) positions[3 * i + 2]);
+			Vector3 vec = new Vector3((float) vectors[3 * i], (float) vectors[3 * i + 1], (float) vectors[3 * i + 2]);
+			this.visualize(origin, vec, scale, vecThickness);
+		}
+	}
+
+	public void visualize(Vector3 position, Vector3 vector, float scale = 1f, float vecDiameter = 0.01f) {
+		Vector3 origin = position + this.offset;
+		Vector3 vec = vector * scale;
+		float vecMag = vec.magnitude;
+		if(vecMag != 0f) {
+			GameObject gameObj = GameObject.CreatePrimitive(PrimitiveType.Cylinder); // Defaults to 2m height, 1m diameter, origin at center.
+			Vector3 objMiddle = origin + vec / 2f;
+			gameObj.transform.position = objMiddle;
+			Vector3 rotAxis = Vector3.Cross(new Vector3(0f, 1f, 0f), vec);
+			float angle = Vector3.SignedAngle(new Vector3(0f, 1f, 0f), vec, rotAxis);
+			gameObj.transform.rotation = Quaternion.AngleAxis(angle, rotAxis);
+			gameObj.transform.localScale = new Vector3(vecDiameter, 0.5f * vecMag, vecDiameter);
+			this.vectorGameObjects.Add(gameObj);
 		}
 	}
 
