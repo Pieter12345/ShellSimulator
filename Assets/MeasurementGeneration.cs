0c4a07576630961adb8876fb6690d349fb548946
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2021-07-11 18:31:15 +0200

    Add line-density measurements generation code

---
+++ Assets/MeasurementGeneration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MeasurementGeneration {
+
+	public static void generateMeasurements() {
+		double sailWidth = 3.5d;
+		double sailHeight = 7d;
+		foreach(string windMag in new string[] {"9.6", "38.4"}) {
+			foreach(string windDeg in new string[] {"30", "45", "60"}) {
+
+				// Load the sail configuration.
+				string fileNameNoEx = "numVerts=561, kLength=500, kArea=500, kBend=0.01, thickness=0.002, density=40," +
+						" steady state with g=9.81, windMag=" + windMag + ", windDeg=" + windDeg;
+				SailConfiguration sailConf = SailConfiguration.loadFromFile(
+						Shell.storageBaseDirPath + "/SailData/" + fileNameNoEx + ".sailshapedata");
+
+				// Generate and store the measurements.
+				string measurementsStorageDir = Shell.storageBaseDirPath + "/SailData/" + fileNameNoEx;
+				foreach(int n in new int[] {1, 5, 10}) {
+					foreach(double m in new double[] {1, 2.5, 5}) {
+						SailMeasurements measurements = new SailMeasurements(sailConf.vertexPositions,
+								MeshUtils.generateTriangularSailMeasurements(
+										sailConf.vertexPositions, sailWidth, sailHeight, n, m));
+						string measurementsFilePath = measurementsStorageDir + "/" + "n=" + n + ", m=" + m + ".measurements";
+						measurements.storeToFile(measurementsFilePath);
+						MonoBehaviour.print("Generated measurements: " + measurementsFilePath);
+					}
+				}
+			}
+		}
+	}
+}
