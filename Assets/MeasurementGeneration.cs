1f0042e271a80e6881f98749b03d4f24a871f418
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2021-10-07 19:44:24 +0200

    Add test 4 measurements generation code

--- Assets/MeasurementGeneration.cs
+++ Assets/MeasurementGeneration.cs
@@ -56,4 +56,30 @@ public class MeasurementGeneration {
 			}
 		}
 	}
+
+	public static void generateTest4Measurements() {
+		double sailWidth = 3.5d;
+		double sailHeight = 7d;
+		foreach(double[] nm in new double[][] {new double[] {5, 2.5}, new double[] {15, 5}}) {
+			int n = (int) nm[0];
+			double m = nm[1];
+			foreach(double noiseMagSlope in new double[] {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0}) {
+
+				// Load the sail configuration.
+				string fileNameNoEx = "numVerts=561,kLength=500,kArea=500,kBend=0.01,thickness=0.002,density=40,"
+						+ "steady state with g=9.81,windMag=38.4,windDeg=60,noiseMagSlope=" + noiseMagSlope;
+				SailConfiguration sailConf = SailConfiguration.loadFromFile(
+						Shell.storageBaseDirPath + "/SailData/test4/" + fileNameNoEx + ".sailshapedata");
+
+				// Generate and store the measurements.
+				string measurementsStorageDir = Shell.storageBaseDirPath + "/SailData/test4/" + fileNameNoEx;
+				SailMeasurements measurements = new SailMeasurements(sailConf.vertexPositions,
+						MeshUtils.generateTriangularSailMeasurements(
+								sailConf.vertexPositions, sailWidth, sailHeight, n, m));
+				string measurementsFilePath = measurementsStorageDir + "/" + "n=" + n + ",m=" + m + ".measurements";
+				measurements.storeToFile(measurementsFilePath);
+				MonoBehaviour.print("Generated measurements: " + measurementsFilePath);
+			}
+		}
+	}
 }
