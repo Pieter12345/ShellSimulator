9a33f0cb99afcadcbc1df05ad41a8f54bf7c23d5
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2022-04-07 01:51:06 +0200

    Fix vertex triangle sorting for more complex meshes
    This has never been a problem for subdivided triangle meshes as the triangles had a convenient order.

--- Assets/MeshUtils.cs
+++ Assets/MeshUtils.cs
@@ -24,49 +24,69 @@ public class MeshUtils {
 			List<int> triangleList = sortedVertexTriangles[vertexInd];
 			if(triangleList.Count > 0) {
 				List<int> sortedTriangleList = new List<int>(triangleList.Count);
+
+				// Insert initial triangle.
 				sortedTriangleList.Add(triangleList[0]);
-				for(int i = 1; i < triangleList.Count; i++) {
+				triangleList.RemoveAt(0);
 
-					// Get triangle vertices.
-					int triangleBaseInd1 = triangleList[i] * 3;
-					int v11 = triangles[triangleBaseInd1];
-					int v12 = triangles[triangleBaseInd1 + 1];
-					int v13 = triangles[triangleBaseInd1 + 2];
+				// Move all triangles from the triangle list to the sorted triangle list.
+				while(triangleList.Count > 0) {
+					bool changed = false;
 
-					// Get the vertex indices of the other vertices that are connected to this vertex's edges.
-					int otherVertexClockwiseInd1 = (vertexInd == v11 ? v13 : (vertexInd == v13 ? v12 : v11));
-					int otherVertexAntiClockwiseInd1 = (vertexInd == v11 ? v12 : (vertexInd == v12 ? v13 : v11));
-
-					// Insert the triangle into the sorted triangles list.
-					bool inserted = false;
-					for(int j = 0; j < sortedTriangleList.Count; j++) {
+					// Insert a triangle that is adjacent to an already added triangle if available.
+					for(int i = 0; i < triangleList.Count; i++) {
 
 						// Get triangle vertices.
-						int triangleBaseInd2 = sortedTriangleList[j] * 3;
-						int v21 = triangles[triangleBaseInd2];
-						int v22 = triangles[triangleBaseInd2 + 1];
-						int v23 = triangles[triangleBaseInd2 + 2];
+						int triangleBaseInd1 = triangleList[i] * 3;
+						int v11 = triangles[triangleBaseInd1];
+						int v12 = triangles[triangleBaseInd1 + 1];
+						int v13 = triangles[triangleBaseInd1 + 2];
 
 						// Get the vertex indices of the other vertices that are connected to this vertex's edges.
-						int otherVertexClockwiseInd2 = (vertexInd == v21 ? v23 : (vertexInd == v23 ? v22 : v21));
-						int otherVertexAntiClockwiseInd2 = (vertexInd == v21 ? v22 : (vertexInd == v22 ? v23 : v21));
+						int otherVertexClockwiseInd1 = (vertexInd == v11 ? v13 : (vertexInd == v13 ? v12 : v11));
+						int otherVertexAntiClockwiseInd1 = (vertexInd == v11 ? v12 : (vertexInd == v12 ? v13 : v11));
 
-						// Insert the triangle before the current triangle if it is placed next to it in anti-clockwise rotation.
-						if(otherVertexClockwiseInd1 == otherVertexAntiClockwiseInd2) {
-							sortedTriangleList.Insert(j, triangleList[i]);
-							inserted = true;
-							break;
+						// Insert the triangle into the sorted triangles list.
+						bool inserted = false;
+						for(int j = 0; j < sortedTriangleList.Count; j++) {
+
+							// Get triangle vertices.
+							int triangleBaseInd2 = sortedTriangleList[j] * 3;
+							int v21 = triangles[triangleBaseInd2];
+							int v22 = triangles[triangleBaseInd2 + 1];
+							int v23 = triangles[triangleBaseInd2 + 2];
+
+							// Get the vertex indices of the other vertices that are connected to this vertex's edges.
+							int otherVertexClockwiseInd2 = (vertexInd == v21 ? v23 : (vertexInd == v23 ? v22 : v21));
+							int otherVertexAntiClockwiseInd2 = (vertexInd == v21 ? v22 : (vertexInd == v22 ? v23 : v21));
+
+							// Insert the triangle before the current triangle if it is placed next to it in anti-clockwise rotation.
+							if(otherVertexClockwiseInd1 == otherVertexAntiClockwiseInd2) {
+								sortedTriangleList.Insert(j, triangleList[i]);
+								triangleList.RemoveAt(i);
+								inserted = true;
+								break;
+							}
+
+							// Insert the triangle after the current triangle if it is placed next to it in clockwise rotation.
+							if(otherVertexAntiClockwiseInd1 == otherVertexClockwiseInd2) {
+								sortedTriangleList.Insert(j + 1, triangleList[i]);
+								triangleList.RemoveAt(i);
+								inserted = true;
+								break;
+							}
 						}
-
-						// Insert the triangle after the current triangle if it is placed next to it in clockwise rotation.
-						if(otherVertexAntiClockwiseInd1 == otherVertexClockwiseInd2) {
-							sortedTriangleList.Insert(j + 1, triangleList[i]);
-							inserted = true;
+						if(inserted) {
+							changed = true;
 							break;
 						}
 					}
-					if(!inserted) {
-						sortedTriangleList.Add(triangleList[i]);
+
+					// Select a random triangle to insert if no adjacent match was found.
+					// This means that there are multiple triangle groups for one vertex.
+					if(!changed) {
+						sortedTriangleList.Add(triangleList[0]);
+						triangleList.RemoveAt(0);
 					}
 				}
 
